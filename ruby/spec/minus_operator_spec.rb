665d56bbcfd1e078bcc37914a724d7f521f44ab4
JulietaAbuin <julieta.abuin@despegar.com>
2020-05-07 20:45:33 -0300

    added minus tests

--- ruby/spec/minus_operator_spec.rb
+++ ruby/spec/minus_operator_spec.rb
@@ -8,5 +8,21 @@ describe CustomTrait do
     it "todoLegal.method1 should return Soy un patito" do
       expect(todoLegal.method1).to eq("Soy un patito")
     end
+
+    class TodoBienTodoLegal
+      uses MyTrait + (MyOtherTrait - :method1) - :method3
+    end
+
+    todoLegal = TodoBienTodoLegal.new
+
+    it "todoLegal should not respond to method3" do
+      expect(todoLegal.respond_to? :method3).to eq false
+    end
+
+    it "todoLegal should raise NoMethodError exception if method3 is called" do
+      expect do
+        todoLegal.method3
+      end.to raise_error(NoMethodError)
+    end
   end
 end
