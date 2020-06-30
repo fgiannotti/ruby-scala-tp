dffe1116c55fdfca6fc5079b5ef327fa968d447e
gebonora <gebonora@gmail.com>
2020-06-30 02:27:27 -0300

    Agrego tests para torneos. Falta equipos.

--- scala/src/test/scala/SpecObjects.scala
+++ scala/src/test/scala/SpecObjects.scala
@@ -1,5 +1,4 @@
-import tp.CriterioAdmision.CriterioAdmision
-import tp.{Arma, BarbaridadMinima, Carrera, Combate, Comestible, Dragon, EsJinete, FuriaNocturna, Gronkle, Item, NadderMortifero, NoSuperaPesoMaximo, Participante, Pesca, PesoMinimo, Posta, PuedeCargarSuPeso, SistemaDeVuelo, SuficienteBarbaridad, TengaItem, TieneArma, TorneoEstandar, Vikingo}
+import tp._
 
 object SpecObjects {
   val hipo: Vikingo = Vikingo(peso = 100, velocidad = 35, barbarosidad = 5, nivelDeHambre = 90, item = SistemaDeVuelo)
@@ -8,20 +7,31 @@ object SpecObjects {
   val astridPesada: Vikingo = Vikingo(peso = 120, velocidad = 40, barbarosidad = 6, nivelDeHambre = 0, item = Arma(40))
   val patan: Vikingo = Vikingo(peso = 79, velocidad = 30, barbarosidad = 3, nivelDeHambre = 0, item = Arma(100))
   val patapez: Patapez.type = Patapez
+  val vikingoLleno: Vikingo = Vikingo(peso = 150, velocidad = 10, barbarosidad = 10, nivelDeHambre = 99, item = Arma(25))
+  val otroVikingoLleno: Vikingo = Vikingo(peso = 155, velocidad = 11, barbarosidad = 15, nivelDeHambre = 99, item = Arma(30))
+  val ragnar: Vikingo = Vikingo(peso = 110, velocidad = 140, barbarosidad = 30, nivelDeHambre = 0, item = Arma(200))
+  val ivar: Vikingo = Vikingo(peso = 60, velocidad = 13, barbarosidad = 3, nivelDeHambre = 1, item = Arma(10))
+  val floki: Vikingo = Vikingo(peso = 80, velocidad = 45, barbarosidad = 300, nivelDeHambre = 0, item = Arma(100))
+
+  val equipoPanzaLlena: Equipo = Equipo(List(vikingoLleno, otroVikingoLleno))
+  val equipoKattegat: Equipo = Equipo(List(ragnar, ivar, floki))
+  val equipoBerg: Equipo = Equipo(List(hipo, astrid, patan, balti))
 
   val combate: Combate = Combate(Some(BarbaridadMinima(3)))
   val combateConArma: Combate = Combate(Some(TieneArma()))
   val pesca: Pesca = Pesca(Some(PesoMinimo(50)))
   val carrera: Carrera = Carrera(12)
-  val carreraConCriterioMontura: Carrera = Carrera(10,Some(EsJinete()))
+  val carreraConCriterioMontura: Carrera = Carrera(10, Some(EsJinete()))
 
 
-  val chimuelo: Dragon = Dragon(100, FuriaNocturna(peso= 500, 250, List(TengaItem(SistemaDeVuelo))))
-  val chimueloRapido: Dragon = Dragon(1000, FuriaNocturna(peso= 500, 250, List(TengaItem(SistemaDeVuelo))))
+  val chimuelo: Dragon = Dragon(100, FuriaNocturna(peso = 500, 250, List(TengaItem(SistemaDeVuelo))))
+  val chimueloRapido: Dragon = Dragon(1000, FuriaNocturna(peso = 500, 250, List(TengaItem(SistemaDeVuelo))))
   val dragonNadder: Dragon = Dragon(70, NadderMortifero(500, List(SuficienteBarbaridad(5))))
+  val furiaNocturna: Dragon = Dragon(1000, FuriaNocturna(600, 500, List(SuficienteBarbaridad(1))))
+  val furiaNocturaSuperDanio = Dragon(1000, FuriaNocturna(650, 3000, List(TengaItem(SistemaDeVuelo))))
 
-  val gronkle:Dragon = Dragon(80,Gronkle(600,List(NoSuperaPesoMaximo(97))))
-  val dragones = List(chimuelo, dragonNadder,gronkle)
+  val gronkle: Dragon = Dragon(80, Gronkle(600, List(NoSuperaPesoMaximo(97))))
+  val dragones = List(chimuelo, dragonNadder, gronkle)
 
   case object Patapez extends Participante {
 
@@ -46,7 +56,8 @@ object SpecObjects {
       this
     }
   }
-  val jugadores = List(hipo,astrid,balti,astridPesada)
+
+  val jugadores = List(hipo, astrid, balti, astridPesada)
 
 }
 
--- scala/src/test/scala/TorneoSpec.scala
+++ scala/src/test/scala/TorneoSpec.scala
@@ -1,6 +1,6 @@
 import SpecObjects._
 import org.scalatest.{FreeSpec, Matchers}
-import tp.{Jinete, TorneoEstandar}
+import tp._
 
 class TorneoSpec extends FreeSpec with Matchers {
 
@@ -12,14 +12,74 @@ class TorneoSpec extends FreeSpec with Matchers {
       "deberia ganar astrid porque es la unica que tiene arma" in {
         torneo1.jugarTorneo shouldBe Some(astrid.copy(nivelDeHambre = astrid.nivelDeHambre + 5))
       }
+    }
 
-      "un torneo con 1 postas de combate" - {
-        val torneo2: TorneoEstandar = new TorneoEstandar(List(carrera), List(chimueloRapido), List(hipo, balti, astrid))
+    "un torneo con 1 postas de carrera" - {
+      val torneo2: TorneoEstandar = new TorneoEstandar(List(carrera), List(chimueloRapido), List(hipo, balti, astrid))
 
-        "deberia ganar astrid porque es la unica que tiene arma" in {
-          torneo2.jugarTorneo shouldBe Some(hipo.copy(nivelDeHambre = hipo.nivelDeHambre + 5))
-        }
+      "deberia ganar hipo porque monta en el chimueloRapido" in {
+        torneo2.jugarTorneo shouldBe Some(hipo.copy(nivelDeHambre = hipo.nivelDeHambre + 5))
       }
     }
+
+    "un torneo estandar con 3 postas de carrera" - {
+      val torneo3: TorneoEstandar = new TorneoEstandar(List(pesca, combateConArma, carrera, carrera), List(chimueloRapido), List(hipo, balti, astrid, astridPesada))
+
+      "deberia ganar astridPesada luego de la primer carrera porque tiene arma y es veloz" in {
+        torneo3.jugarTorneo shouldBe Some(astridPesada.aumentarHambre(15))
+      }
+    }
+
+    "un torneo estandar con 2 vikingos llenos" - {
+      val torneo4: TorneoEstandar = new TorneoEstandar(List(pesca, combateConArma, carrera), List(chimueloRapido), List(vikingoLleno, otroVikingoLleno))
+
+      "no deberia tener ganador porque ninguno puede participar" in {
+        torneo4.jugarTorneo shouldBe None
+      }
+    }
+
+    "un torneo de eliminación de los ultimos 2" - {
+      val torneoEliminacion1: TorneoConEliminacion = TorneoConEliminacion(List(combate, carrera, combateConArma), List(chimueloRapido), List(ragnar, ivar, hipo, patan, astrid, astridPesada), 2)
+      "deberia ganar ragnar en la ronda final" in {
+        torneoEliminacion1.jugarTorneo shouldBe Some(ragnar.aumentarHambre(22))
+      }
+    }
+
+    "un torneo de eliminación de los ultimos 10" - {
+      val torneoEliminacion2: TorneoConEliminacion = TorneoConEliminacion(List(combate, carrera, combateConArma), List(chimueloRapido), List(ragnar, ivar, hipo, patan, astrid, astridPesada), 10)
+      "no deberia tener ganador porque el n es grande y elimina a todos" in {
+        torneoEliminacion2.jugarTorneo shouldBe None
+      }
+    }
+
+    "un torneo inverso con carrera y combate" - {
+      val torneoInverso: TorneoInverso = TorneoInverso(List(carrera, combate), List(chimueloRapido), List(hipo, ragnar, patan, ivar))
+      "deberia ganar ivar porque es el peor" in {
+        torneoInverso.jugarTorneo shouldBe Some(ivar.aumentarHambre(22))
+      }
+    }
+
+    "un torneo con handicap con carrera y un dragón disponible" - {
+      val torneoHandicap1: TorneoHandicap = TorneoHandicap(List(carrera), List(furiaNocturna), List(hipo, patan, astrid, balti, ivar))
+      "deberia ganar ivar es el último de la lista entonces se sube al dragón" in {
+        torneoHandicap1.jugarTorneo shouldBe Some(ivar.aumentarHambre(5))
+      }
+    }
+
+    "un torneo con handicap con carrera, combate y un dragón disponible" - {
+      val torneoHandicap2: TorneoHandicap = TorneoHandicap(List(carrera, combate), List(furiaNocturna), List(hipo, patan, astrid, balti, ivar))
+      "deberia ganar astrid porque es la última en pasar la carrera y entonces se sube al dragón para el combate" in {
+        torneoHandicap2.jugarTorneo shouldBe Some(astrid.aumentarHambre(17))
+      }
+    }
+
+    "un torneo con veto con combate y un dragón disponible que es vetado" - {
+      val torneoConVeto: TorneoConVeto = TorneoConVeto(List(combate), List(furiaNocturaSuperDanio), List(hipo, patan, astrid, balti, ivar), (dragon => dragon.danio < 1000))
+      "deberia ganar patan porque es el mejor guerrero a pie" in {
+        torneoConVeto.jugarTorneo shouldBe Some(patan.aumentarHambre(10))
+      }
+    }
+
+
   }
 }
